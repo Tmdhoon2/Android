f363bc7195cea86b37ba5bbd95e440f0d68e3178
Tmdhoon2 <hks026naver@gmail.com>
2022-05-07 11:38:57 +0900

    +LinearLayout, RelativeLayout

--- MoonNight/app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ MoonNight/app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -8,11 +8,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.FragmentHomeBinding
 
 class   HomeFragment : Fragment(){
-
-
     companion object{
         const val TAG: String = "로그"
 
@@ -20,7 +21,6 @@ class   HomeFragment : Fragment(){
             return HomeFragment()
         }
     }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,11 +32,18 @@ class   HomeFragment : Fragment(){
         Log.d(TAG, "HomeFragment - onAttach() called")
     }
 
+    private lateinit var binding: FragmentHomeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        binding.button1.setOnClickListener {
+            
+        }
 
         Log.d(TAG, "HomeFragment - onCreateView() called")
 
