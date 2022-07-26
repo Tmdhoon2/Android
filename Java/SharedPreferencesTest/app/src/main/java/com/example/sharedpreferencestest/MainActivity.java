613dd2e286587050de9b861ca8c2dc7f0ca96b66
Tmdhoon_2 <hks026naver@gmail.com>
2022-07-26 23:22:42 +0900

    팝업창 예제 추가

--- Java/SharedPreferencesTest/app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
+++ Java/SharedPreferencesTest/app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.sharedpreferencestest;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btLogin;
     private TextView tvId;
     private TextView tvPw;
+    private Button btPop;
 
     private SharedPreferences preferences;
     private SharedPreferences.Editor editor;
@@ -30,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         btLogin = findViewById(R.id.btLogin);
         tvId = findViewById(R.id.tvId);
         tvPw = findViewById(R.id.tvPw);
+        btPop = findViewById(R.id.btPop);
 
         preferences = getSharedPreferences("UserInfo", MODE_PRIVATE);
 
@@ -44,6 +49,34 @@ public class MainActivity extends AppCompatActivity {
 
                 tvId.setText(preferences.getString("Id", ""));
                 tvPw.setText(preferences.getString("Pw", ""));
+            }
+        });
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+
+        btPop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                builder.setTitle("제목");
+                builder.setMessage("메시지");
+                
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Yes", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "No", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         });
 
