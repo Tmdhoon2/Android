998aa5d3a00f22a5e754fe6af1d24247e8b1d3c5
Tmdhoon2 <hks026naver@gmail.com>
2022-09-07 17:05:49 +0900

    💡 :: Code review

--- Kotlin/ViewModelExample/app/src/main/java/com/tmdhoon/livedata/MainActivity.kt
+++ Kotlin/ViewModelExample/app/src/main/java/com/tmdhoon/livedata/MainActivity.kt
@@ -10,6 +10,7 @@ import com.tmdhoon.livedata.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    // 뷰모델 연결
     private lateinit var userViewModel: UserViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,9 +18,12 @@ class MainActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        // ViewModelProvider 를 통해 Viewmodel 객체 획득
         userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        // xml 과 userViewModel 연결
         binding.user = userViewModel
 
+        // 옵저버로 객체 지정
         val nameObserver = Observer<Int> { it ->
             binding.textViewHeight.text = it.toString();
         }
--- Kotlin/ViewModelExample/app/src/main/java/com/tmdhoon/livedata/UserViewModel.kt
+++ Kotlin/ViewModelExample/app/src/main/java/com/tmdhoon/livedata/UserViewModel.kt
@@ -5,15 +5,21 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class UserViewModel() : ViewModel() {
+    // height 값을 관찰 가능한 데이터로 만들어줌
+    // 수정 가능
     private var _height = MutableLiveData<Int>()
 
+    // 수정 불가능한 라이브 데이터
     val height: LiveData<Int>
+    // kotlin getter
     get() = _height
 
+    // 생성자
     init {
         _height.value = 170
     }
 
+    // height 증가 함수
     fun increase(){
         _height.value = _height.value?.plus(1)
     }
