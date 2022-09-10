767cdfc664c12728ab877180c0e05889bb26cebe
Tmdhoon_2 <hks026naver@gmail.com>
2022-09-10 21:50:19 +0900

    📝 :: Second Example

--- Kotlin/MutableLiveData/app/src/main/java/com/example/mutablelivedata/MainActivity.kt
+++ Kotlin/MutableLiveData/app/src/main/java/com/example/mutablelivedata/MainActivity.kt
@@ -6,14 +6,18 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
-import com.example.myapplication.R
+import com.example.mutablelivedata.R
 
 class MainActivity : AppCompatActivity() {
     private lateinit var button1 : Button
     private lateinit var button2 : Button
     private lateinit var textView : TextView
+    private lateinit var button3 : Button
+    private lateinit var button4 : Button
+    private lateinit var textView2 : TextView
+
     private var liveData : MutableLiveData<String> = MutableLiveData()
-    private var text = "Hello world!"
+    private var liveData2 : MutableLiveData<String> = MutableLiveData()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +27,16 @@ class MainActivity : AppCompatActivity() {
         button2 = findViewById(R.id.button2)
         textView = findViewById(R.id.textView)
 
+        textView2 = findViewById(R.id.textView2)
+        button3 = findViewById(R.id.button3)
+        button4 = findViewById(R.id.button4)
+
         liveData.observe(this, Observer{
             textView.text = it
+        })
+
+        liveData2.observe(this, Observer {
+            textView2.text = it
         })
 
         button1.setOnClickListener {
@@ -33,6 +45,14 @@ class MainActivity : AppCompatActivity() {
 
         button2.setOnClickListener {
             liveData.value = "Hi hell"
+        }
+
+        button3.setOnClickListener {
+            liveData2.value = "Hello world!"
+        }
+
+        button4.setOnClickListener {
+            liveData2.value = "Hi hell"
         }
 
     }
